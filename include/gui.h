cf4dc1c511e374e6c72829a8e4742300a2e60390
Pedro M. Dominguez <84252067+xtcedro@users.noreply.github.com>
2024-11-19 23:04:46 -0600

    Create gui.h

---
+++ include/gui.h
@@ -0,0 +1,10 @@
+#ifndef GUI_H
+#define GUI_H
+
+// Initializes and starts the GUI
+void start_gui(int argc, char *argv[]);
+
+// Updates the display with the provided text
+void update_display(const char *text);
+
+#endif // GUI_H
